2c638651aefd8343e05375168fe6e0e4ddc6008e
carlavarea <carla.varea@est.fib.upc.edu>
2018-04-13 14:34:37 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :submits
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#welcome'
+  root 'submits#index'
 
 end
