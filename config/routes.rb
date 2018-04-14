2bc86b9546c8d89a33dc6e80d2f240578104ebbe
carlavarea <carla.varea@est.fib.upc.edu>
2018-04-14 14:16:04 +0000

    master

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :users
   resources :contributions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'submits#index'
+  root 'contributions#index'
+  
+  #get 'submit', to: 'contributions#new'
+  #post 'submit', to: 'contributions#create'
 
 end
