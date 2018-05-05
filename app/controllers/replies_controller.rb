259ce47ea89b5a1780d7e5831adbf3fdbafe15c4
Alex Huang Feng <alexhuang05@gmail.com>
2018-05-05 22:41:26 +0200

    not working

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,16 +1,41 @@
 class RepliesController < ApplicationController
 
+  def createWithParent
+    @parentReply = Reply.find(reply_params[:parent_id])
+    @comment = @parentReply.comment
+    commentID = @comment.id
+    # {"utf8"=>"✓",
+    #   "authenticity_token"=>"eCG5eL7LJzYkBElyVK/xqKW5pmQgvLunf5s+3+Xh2N8WFyXlARbyEJSx628fPcS1Cck15cUzTBQH8RlIOQKNKA==",
+    #   "reply"=>{"body"=>"asdf"},
+    #   "button"=>"",
+    #   "contribution_id"=>"3",
+    #   "comment_id"=>"4"}
+    @reply = @parentReply.replies.new(user: current_user, body: reply_params[:body])
+    if @reply.save
+      @comment.contribution.upComments()
+      @comment.contribution.save
+      redirect_to "/contributions/"+params[:contribution_id], notice: 'Reply created'
+    else
+      redirect_to "/comments/"+commentID, notice: 'Reply was not saved. Ensure you have entered a Reply'
+    end
+  end
+
   def create
     @comment = Comment.find(params[:comment_id])
     @contribution = @comment.contribution
     @reply = @comment.replies.new(user: current_user, body: reply_params[:body])
-    @comment.contribution.upComments()
-    @comment.contribution.save
     if @reply.save
+      @comment.contribution.upComments()
+      @comment.contribution.save
       redirect_to "/contributions/"+params[:contribution_id], notice: 'Reply created'
     else
       redirect_to "/comments/"+params[:comment_id], notice: 'Reply was not saved. Ensure you have entered a Reply'
     end
+  end
+
+  def show 
+    @newreply = Reply.new
+    @reply = Reply.find(params[:id])
   end
 
   private
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,4 +2,6 @@ class Reply < ApplicationRecord
   validates :body, presence: true
   belongs_to :user
   belongs_to :comment
+  belongs_to :parent,  class_name: "Reply" #-> requires "parent_id" column
+  has_many   :replies, class_name: "Reply", foreign_key: :parent_id, dependent: :destroy
 end
