4b851a59fb8a7ad4c464ef4e965caaedf214c6ad
Alex Huang Feng <alexhuang05@gmail.com>
2018-05-12 17:42:06 +0200

    DELETE .../replies/id fet

--- app/controllers/api/v1/replies_controller.rb
+++ app/controllers/api/v1/replies_controller.rb
@@ -122,7 +122,22 @@ class Api::V1::RepliesController <  Api::V1::ApiController
     if !@current_user
       send_unauthorized
     else
-      notImplemented
+      @contribution = Contribution.find_by_id(params[:contribution_id])
+      if @contribution.nil?
+        render json: { message: "This contribution doesn't exist"}, status: 404 and return
+      else
+        @comment = @contribution.comments.find_by_id(params[:comment_id])
+        if @comment.nil?
+          render json: { message: "This comment doesn't exist"}, status: 404 and return
+        end
+        @reply = @comment.replies.find_by_id(params[:id])
+        if @reply.nil?
+          render json: { message: "This reply doesn't exist."}, status: 404 and return
+        end
+        @reply.replies.destroy_all
+        @reply.destroy
+        render json: { message: "This reply has been deleted"}, status: 200 and return
+      end
     end
   end
 
