5d5b03231cbe92612855512f0c430bb5de3dae3c
Alex Huang Feng <alexhuang05@gmail.com>
2018-06-01 23:55:48 +0200

    hasVoted att to contribution

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ApiController < ActionController::Base
 
+  serialization_scope :view_context
+
   def current_user 
     @current_user = ::User.find_by_api_key(request.headers["Authorization"])
   end
--- app/serializers/contribution_simple_serializer.rb
+++ app/serializers/contribution_simple_serializer.rb
@@ -2,11 +2,15 @@ require 'action_view'
 require 'action_view/helpers'
 include ActionView::Helpers::DateHelper
 class ContributionSimpleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :text, :username, :user_id, :created_at, :numComments, :points
+  attributes :id, :title, :url, :text, :username, :user_id, :created_at, :numComments, :points, :hasVoted
 
   def username
     #object es el this o self en C
     object.user.name
+  end
+
+  def hasVoted
+    scope.current_user.hasvoted?(object)
   end
   
   def created_at
